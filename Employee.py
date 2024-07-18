f91861659d74dff7cf69d113d1fc5431ba16861c
Devin VonTickner <devinvt03@gmail.com>
2024-07-17 20:45:06 -0700

    Finished __init__

--- Employee.py
+++ Employee.py
@@ -1,2 +1,6 @@
 class Employee:
-    def __init__(self):
+    def __init__(self, name, position):
+        self.cars_sold = 0
+        self.revenue_generated = 0
+        self.name = name
+        self.position = position
